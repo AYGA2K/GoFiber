f2b18dfcb9aaddf7e09b2008ad0114a53f959064
Ayga2k <garouatayoub@gmail.com>
2023-02-09 19:20:56 +0100

    auth middleware is done

--- middleware/auth.go
+++ middleware/auth.go
@@ -2,54 +2,17 @@ package middleware
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/joho/godotenv"
 )
 
-type User struct {
-	// This is not the model, more like a serializer
-	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
-}
 type Claims struct {
 	ID    uint   `json:"id"`
 	Email string `json:"email"`
 	jwt.StandardClaims
-}
-
-func GenerateJWT(token_type string, user User, expiry time.Duration) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":    user.ID,
-		"email": user.Email,
-		"exp":   time.Now().Add(expiry).Unix(),
-	})
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	if token_type == "access" {
-		// Generate encoded token and send it as response.
-		access := os.Getenv("ACCESS_KEY")
-
-		tokenString, err := token.SignedString([]byte(access))
-
-		return tokenString, err
-	}
-	if token_type == "refresh" {
-		// Generate encoded token and send it as response.
-		refresh := os.Getenv("REFRESH_KEY")
-
-		tokenString, err := token.SignedString([]byte(refresh))
-
-		return tokenString, err
-	}
-	return "", nil
 }
 
 func AuthMiddleware(c *fiber.Ctx) error {
